f1e1b4b61c40a7d344878ddc119f1660ffb31047
Jorrit Tempelman <jorrit.tempelman@inter.ikea.com>
2026-05-16 18:45:54 +0200

    feat: update hero text

--- src/content/en/hero.ts
+++ src/content/en/hero.ts
@@ -1,5 +1,5 @@
 export const hero = {
-  title: "Classic Swedish holiday home without classic inconveniences",
+  title: "Classic Swedish holiday home with modern features",
   description:
     "Built in 2012, with fast internet and great insulation. Right in the middle of the forests of Småland, southern Sweden, all yours for just €145,000.",
   ctas: {
--- src/content/nl/hero.ts
+++ src/content/nl/hero.ts
@@ -1,5 +1,5 @@
 export const hero = {
-  title: "Klassiek Zweeds vakantiehuis zonder klassieke ongemakken",
+  title: "Klassiek Zweeds vakantiehuis met moderne features",
   description:
     "Gebouwd in 2012, met een snelle internetverbinding en een goede isolatie. Midden in de bossen van Småland, Zuid-Zweden, helemaal voor jullie voor slechts €145.000.",
   ctas: {
